e5954b57984f7375a6f6a3dbb76eed208a2510bc
faisal <fmohd743@gmail.com>
2024-03-23 12:53:52 +0530

    second largest

---
+++ CountDistinctChar.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CountDistinctChar {
+    public static void main(String[] args) {
+        System.out.println("Enter the string:");
+        Scanner sc=new Scanner(System.in);
+        String Str1=sc.nextLine();
+        System.out.println(Str1);
+    }
+}
---
+++ findSecondLargest.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class findSecondLargest {
+    public static void main(String[] args) {
+        System.out.println("Enter value to insert in list");
+        List<Integer> inputVal=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        int val=sc.nextInt();
+        inputVal.add(val);
+        System.out.println(inputVal);
+
+
+    }
+}
