9cc637000812372fe3c873d9920afa650a725ff1
faisal <fmohd743@gmail.com>
2024-03-27 10:48:00 +0530

    distinct element

--- CountDistinctChar.java
+++ CountDistinctChar.java
@@ -1,10 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
 public class CountDistinctChar {
     public static void main(String[] args) {
         System.out.println("Enter the string:");
         Scanner sc=new Scanner(System.in);
         String Str1=sc.nextLine();
-        System.out.println(Str1);
+         
+
+        DistinctElement dst=new DistinctElement();
+        List<Character> str=dst.distinctChar(Str1);
+        System.out.println(str);
+    
+
     }
-}
+
+    static class DistinctElement{
+        public static List<Character> distinctChar(String str){
+            List<Character> distinct=new ArrayList<>();
+            for(char ch :str.toCharArray()){
+                if(!distinct.contains(ch)){
+                    distinct.add(ch);
+                   
+                }
+                
+            }
+            return distinct;
+
+        }
+    }
+    }
+
+    
