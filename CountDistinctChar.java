efb8b3f088a19723b9461bbed94fcd43cfd2e341
faisal <fmohd743@gmail.com>
2024-03-27 11:01:41 +0530

    Now you can convert char list into String

--- CountDistinctChar.java
+++ CountDistinctChar.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,15 +13,19 @@ public class CountDistinctChar {
         List<Character> str=dst.distinctChar(Str1);
         System.out.println(str.toString());
         
-       
+    //    Method 1;
      StringBuilder sb=new StringBuilder(str.size());
      for (Character ch :str){
         sb.append(ch);
      }
      String newStr=sb.toString();
      System.out.println(newStr);
-
+      // Method 2 for converting into String
+      String newStr2 = str.stream().collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
+        System.out.println("Concatenated string : " + newStr2);
     }
+   
+  
 
     static class DistinctElement{
         public static List<Character> distinctChar(String str){
