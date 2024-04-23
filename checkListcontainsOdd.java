97f4c0e68c117995b1adf9eecb3431b91e1bc390
faisal <fmohd743@gmail.com>
2024-04-23 11:58:01 +0530

    find oddlist

---
+++ checkListcontainsOdd.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class checkListcontainsOdd {
+    public static void main(String[] args) {
+        List<Integer> oddlist=new ArrayList<>();
+        oddlist.add(10);
+        Scanner sc=new Scanner(System.in);
+        boolean n=true;
+        while(n){
+            System.out.println("Enter the number which you want to add in list");
+            int val=sc.nextInt();
+            if(val!=0){
+            oddlist.add(val);
+            }
+            else{
+                n=false;
+            }
+            for(int   l :oddlist){
+                System.out.println("this is your list"+l);
+
+            }
+            
+        }
+
+      
+
+        
+    }
+    
+}
