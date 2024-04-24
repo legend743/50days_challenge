0073c7419f3f3a950c3480f115bb40e71eb0de2c
faisal <fmohd743@gmail.com>
2024-04-24 12:07:52 +0530

    sort an array

---
+++ sortAnarray.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class sortAnarray {
+    public static void main(String[] args) {
+        System.out.println("Enter the array");
+        int [] arr={10,2,3,55,45,8,99};
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
