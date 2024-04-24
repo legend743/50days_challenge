19df8c2cfbd847d205a056e348a60891325d34e9
faisal <fmohd743@gmail.com>
2024-04-24 12:27:45 +0530

    Hashmap

---
+++ CreateDeadLock.java
@@ -0,0 +1,12 @@
+public class CreateDeadLock {
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("you are under certain condition");
+            while (true) {
+                System.out.println("yes");
+                
+            }
+
+        }
+    }
+}
---
+++ sortHashmap.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class sortHashmap {
+    public static void main(String[] args) {
+            Map<String,Integer> scores=new HashMap<>();
+            scores.put("Raja",10);
+            scores.put("Javed", 198);
+            scores.put("Malinga", 0);
+            System.out.println("scores"+scores);
+
+
+    }
+}
